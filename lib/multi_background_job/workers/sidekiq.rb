96807662da7cf733da6cb44d557f7cf147e60c17
Marcos G. Zimmermann <mgzmaster@gmail.com>
2020-10-05 21:59:43 -0300

    adds a new API to interact with worker classes by adapter (#5)

---
+++ lib/multi_background_job/workers/sidekiq.rb
@@ -0,0 +1,26 @@
+# frizen_string_literal: true
+
+require_relative './shared_class_methods'
+
+module MultiBackgroundJob
+  module Workers
+    module Sidekiq
+      def self.extended(base)
+        base.include(::Sidekiq::Worker) if defined?(::Sidekiq)
+        base.extend SharedClassMethods
+        base.extend ClassMethods
+      end
+
+      module ClassMethods
+        def service_worker_options
+          default_queue = ::Sidekiq.default_worker_options['queue'] if defined?(::Sidekiq)
+          default_retry = ::Sidekiq.default_worker_options['retry'] if defined?(::Sidekiq)
+          {
+            queue: (default_queue || 'default'),
+            retry: (default_retry || 15),
+          }
+        end
+      end
+    end
+  end
+end
